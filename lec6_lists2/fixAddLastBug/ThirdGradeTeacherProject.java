18b88cea13b60b77ef4cf466938716f369cf8599
Josh Hug <hug@cs.berkeley.edu>
2026-02-02 13:48:49 -0800

    starter code for lecture 6 challenges

---
+++ lec6_lists2/fixAddLastBug/ThirdGradeTeacherProject.java
@@ -0,0 +1,8 @@
+package lec4_lists2.fixAddLastBug;
+
+public class ThirdGradeTeacherProject {
+    public static void main(String[] args) {
+        SLList L = new SLList(10);
+        L.addFirst(4);
+    }
+}
