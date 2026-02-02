3a76d7f6cbad7e4460f0142ac7b24cbcf3bf548b
Josh Hug <hug@cs.berkeley.edu>
2026-02-02 13:49:40 -0800

    remove code created during a lecture last year

--- lec6_lists2/fixAddLastBug/ThirdGradeTeacherProject.java
+++
@@ -1,8 +0,0 @@
-package lec4_lists2.fixAddLastBug;
-
-public class ThirdGradeTeacherProject {
-    public static void main(String[] args) {
-        SLList L = new SLList(10);
-        L.addFirst(4);
-    }
-}
