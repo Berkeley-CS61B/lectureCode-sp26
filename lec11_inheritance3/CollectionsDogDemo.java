ccae19ff4d021da6ddbf66f1ae6c50d834d7ae59
Josh Hug <hug@cs.berkeley.edu>
2026-02-13 14:44:08 -0800

    lecture 11 code

--- lec11_inheritance3/CollectionsDogDemo.java
+++ lec11_inheritance3/CollectionsDogDemo.java
@@ -1,4 +1,4 @@
-package lec10_inheritance3;
+package lec11_inheritance3;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,13 +7,18 @@ import java.util.List;
 public class CollectionsDogDemo {
     public static void main(String[] args) {
         List<Dog> dogs = new ArrayList<>();
+
         dogs.add(new Dog("Grigometh", 200));
         dogs.add(new Dog("Pelusa", 5));
         dogs.add(new Dog("Clifford", 9000));
 
-        // Dog maxDog = Collections.max(dogs);
-        // Dog maxNameDog = Collections.max(dogs, new Dog.NameComparator());
-        
+        // operator overloading in Java does not exist
+
+
+        Dog maxDog = Collections.max(dogs);
+
+        Dog maxByName = Collections.max(dogs, Dog.NAME_COMPARATOR);
+
         // Not in scope for 61B but you can also use a lambda
         // expression in Java to define a Comparator rather than a 
         // separate class. Lambdas in Java are done using ->
